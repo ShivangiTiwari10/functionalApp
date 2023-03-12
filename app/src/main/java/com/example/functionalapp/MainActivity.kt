d05aed199178192b4f40ab9c926221e1c4d8d464
Shivangi <shivangit1111@gmail.com>
2023-03-12 21:02:35 +0530

    feat :make activity

--- app/src/main/java/com/example/functionalapp/MainActivity.kt
+++ app/src/main/java/com/example/functionalapp/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnGo.setOnClickListener {
+
+        }
         setContentView(binding.root)
     }
 }
