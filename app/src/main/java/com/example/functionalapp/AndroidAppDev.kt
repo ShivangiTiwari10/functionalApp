1e1da0eab51384417b91bc83bf8bd6b55bafbbe1
Shivangi <shivangit1111@gmail.com>
2023-03-15 17:49:30 +0530

    feat : send user to webPage

--- app/src/main/java/com/example/functionalapp/AndroidAppDev.kt
+++ app/src/main/java/com/example/functionalapp/AndroidAppDev.kt
@@ -1,5 +1,7 @@
 package com.example.functionalapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.functionalapp.databinding.ActivityAndroidAppDevBinding
@@ -11,6 +13,13 @@ class AndroidAppDev : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityAndroidAppDevBinding.inflate(layoutInflater)
+
+        binding.kotlin.setOnClickListener {
+            val webIntent: Intent = Uri.parse("https://developer.android.com/kotlin").let { webpage ->
+                Intent(Intent.ACTION_VIEW, webpage)
+            }
+            startActivity(webIntent)
+        }
         setContentView(binding.root)
     }
 }
